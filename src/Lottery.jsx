0cebc766db76af28af01c0b51659a9313804916a
Prabal Singh Bhadoriya <129841865+Prabal0002@users.noreply.github.com>
2024-08-09 11:11:15 +0530

    Update Lottery.jsx

--- src/Lottery.jsx
+++ src/Lottery.jsx
@@ -1,1 +1,31 @@
+import { useState } from "react";
+import "./Lottery.css";
+import  {genTicket , sum} from "./helper";
+import"./Lottery.css";
+import Ticket from "./Ticket.jsx";
 
+
+
+export default function Lottery({num, WinningSum}) {
+let [ticket, setTicket] =useState (genTicket(num));
+  
+let isWinning = sum(ticket) === WinningSum;
+  
+let buyTicket = () => {
+  setTicket (genTicket(num));
+};
+
+
+return(
+<div>
+<h1>Lottery Game!</h1>
+  <Ticket ticket={ticket}/>
+
+<button onClick={buyTicket}> Buy New Ticket</button>
+
+<h3>-{isWinning && "Congratulations, you won!"}</h3>  
+</div>
+
+);
+
+}
