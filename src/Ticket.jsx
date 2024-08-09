b7e47de3cd282ef4848ee103ca79bb6f4385eeb6
Prabal Singh Bhadoriya <129841865+Prabal0002@users.noreply.github.com>
2024-08-09 11:14:20 +0530

    Update Ticket.jsx

--- src/Ticket.jsx
+++ src/Ticket.jsx
@@ -1,1 +1,21 @@
+import TicketNum from "./TicketNum.jsx";
 
+import "./Ticket.css";
+
+export default function Ticket({ ticket }) {
+
+return (
+
+<div className="Ticket"> I
+
+<p>Ticket</p>
+
+{ticket.map((num, idx) =>(
+
+<TicketNum num={num} key={idx} />
+
+))}
+
+</div>
+  );
+}
